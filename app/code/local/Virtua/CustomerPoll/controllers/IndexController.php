dc03609f3e8a5df333202c507017851f2ffdf83c
Virtua <m.skalny@wearevirtua.com>
2018-09-03 13:30:37 +0200

    VIMS-67

---
+++ app/code/local/Virtua/CustomerPoll/controllers/IndexController.php
@@ -0,0 +1,9 @@
+<?php
+
+class Virtua_CustomerPoll_controllers_IndexController extends Mage_Core_Controller_Front_Action
+{
+    public function indexAction()
+    {
+        echo "Hello World";
+    }
+}
