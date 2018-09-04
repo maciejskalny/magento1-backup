8165ef307a6f14a161b24ce8834f3be754ad43b9
Virtua <m.skalny@wearevirtua.com>
2018-09-04 10:18:13 +0200

    VIMS-67, creating template

--- app/code/local/Virtua/CustomerPoll/Block/CustomerPoll.php
+++ app/code/local/Virtua/CustomerPoll/Block/CustomerPoll.php
@@ -4,6 +4,12 @@ class Virtua_CustomerPoll_Block_CustomerPoll extends Mage_Core_Block_Template
 {
     public function myfunction()
     {
-        return "Hello word";
+        $vote = Mage::app()->getRequest()->getParam('storepoll');
+
+        if (!is_null($vote)) {
+            if ($vote == "yes") {
+            } else {
+            }
+        }
     }
 }
