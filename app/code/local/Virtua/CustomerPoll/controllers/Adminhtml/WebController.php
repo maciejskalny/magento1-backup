9f31ab67025cd28be5c97a0ef5755c9f3a0e3854
Virtua <m.skalny@wearevirtua.com>
2018-09-05 13:03:23 +0200

    VIMS-67, testing bar char example

--- app/code/local/Virtua/CustomerPoll/controllers/Adminhtml/WebController.php
+++ app/code/local/Virtua/CustomerPoll/controllers/Adminhtml/WebController.php
@@ -4,14 +4,36 @@ class Virtua_CustomerPoll_Adminhtml_WebController extends Mage_Adminhtml_Control
 {
     public function indexAction()
     {
-//        $this->loadLayout();
-//        $this->renderLayout();
-//
-//        echo 'HelloWord';
-
         $this->loadLayout()
             ->_setActiveMenu('customermenu')
             ->_title($this->__('Poll'));
         $this->renderLayout();
+
+        $model = Mage::getModel('customerpoll/customerpoll');
+
+        Zend_Debug::dump($model->load(1));
     }
+
+//    public function indexxAction()
+//    {
+//        $this->loadLayout();
+//        $this->renderLayout();
+//
+//        $model = Mage::getModel('customerpoll/customerpoll');
+//        $vote = Mage::app()->getRequest()->getParam('storepoll');
+//
+//        if ($vote == 'yes' || $vote == 'no') {
+//            if (empty($model->load($vote, 'option')->getOrigData())) {
+//                $data = array(
+//                    'option' => $vote,
+//                    'count' => 1
+//                );
+//                $model->setData($data)->save();
+//            } else {
+//                $count = (int)$model->load($vote, 'option')->getOrigData()['count'];
+//                $count = $count + 1;
+//                $model->load($vote, 'option')->setData('count', $count)->save();
+//            }
+//        }
+//    }
 }
