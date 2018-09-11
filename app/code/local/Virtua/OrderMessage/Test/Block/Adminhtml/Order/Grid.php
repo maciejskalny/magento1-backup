47add0397c032c783eb88938e421f46ca83b8a86
Virtua <m.skalny@wearevirtua.com>
2018-09-11 09:04:11 +0200

    VIMS-68, added test mesage column to orders grid

---
+++ app/code/local/Virtua/OrderMessage/Test/Block/Adminhtml/Order/Grid.php
@@ -0,0 +1,22 @@
+<?php
+
+/**
+ * This file is responsible for joining message column to orders grid.
+ *
+ * PHP version 7.1.21
+ *
+ * @category  Module
+ * @package   Virtua_Internship
+ * @author    Maciej Skalny <contact@wearevirtua.com>
+ * @copyright 2018 Copyright (c) Virtua (http://wwww.wearevirtua.com)
+ * @license   GPL http://opensource.org/licenses/gpl-license.php
+ * @link      https://bitbucket.org/wearevirtua/magento1ms/
+ */
+
+/**
+ * Class Virtua_OrderMessage_Test_Block_Adminhtml_Order_Grid
+ */
+class Virtua_OrderMessage_Test_Block_Adminhtml_Order_Grid extends Mage_Adminhtml_Block_Sales_Order_Grid
+{
+
+}
