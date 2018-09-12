3f1bcdfe08be9dd65e7c9fd69cc57c2c1f20ebaa
Virtua <m.skalny@wearevirtua.com>
2018-09-12 11:00:08 +0200

    VIMS-68, adding topics to message form in checkout

--- app/code/local/Virtua/OrderMessage/Block/OrderMessage.php
+++ app/code/local/Virtua/OrderMessage/Block/OrderMessage.php
@@ -6,4 +6,10 @@ class Virtua_OrderMessage_Block_OrderMessage extends Mage_Core_Block_Template
     {
         echo "Hello World!";
     }
+
+    public function prepareTopics()
+    {
+        $topics = Mage::getModel('ordermessage/ordermessagetopic')->getCollection()->getData();
+        return $topics;
+    }
 }
