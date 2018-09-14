bf728aefd5f5d91bb59417b1f3394349ab08b359
Virtua <m.skalny@wearevirtua.com>
2018-09-14 08:46:43 +0200

    VIMS-68, cleaning some code

--- app/code/local/Virtua/OrderMessage/Block/Adminhtml/OrderMessage/Edit.php
+++ app/code/local/Virtua/OrderMessage/Block/Adminhtml/OrderMessage/Edit.php
@@ -11,29 +11,6 @@ class Virtua_OrderMessage_Block_Adminhtml_OrderMessage_Edit extends Mage_Adminht
         $newOrEdit = $this->getRequest()->getParam('id')
             ? $this->__('Edit')
             : $this->__('New');
-        $this->_headerText = $newOrEdit . ' ' . $this->__('Brand');
+        $this->_headerText = $newOrEdit . ' ' . $this->__('Topic');
     }
 }
-//<?php
-//class SmashingMagazine_BrandDirectory_Block_Adminhtml_Brand_Edit
-//    extends Mage_Adminhtml_Block_Widget_Form_Container
-//{
-//    protected function _construct()
-//    {
-//        $this->_blockGroup = 'smashingmagazine_branddirectory_adminhtml';
-//        $this->_controller = 'brand';
-//
-//        /**
-//         * The $_mode property tells Magento which folder to use
-//         * to locate the related form blocks to be displayed in
-//         * this form container. In our example, this corresponds
-//         * to BrandDirectory/Block/Adminhtml/Brand/Edit/.
-//         */
-//        $this->_mode = 'edit';
-//
-//        $newOrEdit = $this->getRequest()->getParam('id')
-//            ? $this->__('Edit')
-//            : $this->__('New');
-//        $this->_headerText =  $newOrEdit . ' ' . $this->__('Brand');
-//    }
-//}
--- app/code/local/Virtua/OrderMessage/Block/OrderMessage.php
+++ app/code/local/Virtua/OrderMessage/Block/OrderMessage.php
@@ -2,11 +2,6 @@
 
 class Virtua_OrderMessage_Block_OrderMessage extends Mage_Core_Block_Template
 {
-    public function helloWorld()
-    {
-        echo "Hello World!";
-    }
-
     public function prepareTopics()
     {
         $topics = Mage::getModel('ordermessage/ordermessagetopic')->getCollection()->getData();
