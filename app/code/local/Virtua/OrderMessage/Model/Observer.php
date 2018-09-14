bbc7779c1970855464e733597b682ddc3db74789
Virtua <m.skalny@wearevirtua.com>
2018-09-14 11:59:08 +0200

    VIMS-68, working on adding order message by observer

--- app/code/local/Virtua/OrderMessage/Model/Observer.php
+++ app/code/local/Virtua/OrderMessage/Model/Observer.php
@@ -2,12 +2,35 @@
 
 class Virtua_OrderMessage_Model_Observer extends Varien_Event_Observer
 {
+    /**
+     *
+     * This function is called when $order->load() is done.
+     * Here we read our custom fields value from database and set it in order object.
+     * @param unknown_type $evt
+     */
     public function saveOrderMessage($observer)
     {
-        $event = $observer->getEvent();
-        $model = $event->getPage();
+        Mage::log(Mage::app()->getRequest()->getParams());
+//        $order = $evt->getOrder();
+//        $model = Mage::getModel('custom/custom_order');
+//        $data = $model->getByOrder($order->getId());
+//        foreach($data as $key => $value){
+//            $order->setData($key,$value);
+//        }
 
-        Zend_Debug::dump($model->getData());
-        die();
+        $order = $observer->getEvent()->getOrder();
+        $orderIds = $order->getId();
+
+        Mage::log($orderIds);
+
+        $data = array(
+            'order_id' => $orderIds,
+            'customer_id' => 2,
+            'topic_id' => 3,
+            'message' => 'To jest testowa wiadomosc'
+        );
+
+        $model = Mage::getModel('ordermessage/ordermessage');
+        $model->setData($data)->save();
     }
 }
