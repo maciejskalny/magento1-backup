5e2d6fa0b0b1c82ddcbdf8e861c8bfd1ef8f1d27
Virtua <m.skalny@wearevirtua.com>
2018-09-14 14:43:16 +0200

    VIMS-68, adding custom order fields to db

--- app/code/local/Virtua/OrderMessage/Model/Observer.php
+++ app/code/local/Virtua/OrderMessage/Model/Observer.php
@@ -15,14 +15,17 @@ class Virtua_OrderMessage_Model_Observer extends Varien_Event_Observer
         $order = $observer->getEvent()->getOrder();
 
         $orderId = $order->getId();
-        $orderCustomer = $order->getCustomerId();
+        $customerId = $order->getCustomerId();
+        $topicId = Mage::app()->getRequest()->getParam('ordermessage-topic', '');
+        $message = Mage::app()->getRequest()->getParam('ordermessage-message', '');
 
-        Mage::log(Mage::app()->getRequest()->getParams(), null, 'system.log', true);
+        //Mage::log(Mage::app()->getRequest()->getParams(), null, 'system.log', true);
         //Mage::log($test, null, 'system.log', true);
 
         //$param = Mage::app()->getRequest()->getParam('namespace_reviewfield', '');
 
-        $param = Mage::app()->getRequest()->getParam('namespace_reviewfield', '');
+
+
         //$observer->getEvent-getOrder()->setMyField((string) $param);
 
 
@@ -30,9 +33,9 @@ class Virtua_OrderMessage_Model_Observer extends Varien_Event_Observer
 
         $data = array(
             'order_id' => $orderId,
-            'customer_id' => $orderCustomer,
-            'topic_id' => 3,
-            'message' => $param
+            'customer_id' => $customerId,
+            'topic_id' => $topicId,
+            'message' => $message
         );
 
         $model = Mage::getModel('ordermessage/ordermessage');
