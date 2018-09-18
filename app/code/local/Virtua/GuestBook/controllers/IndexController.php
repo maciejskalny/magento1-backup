bd0d23f55833393091385ce0af3b6f951a5f4612
Virtua <m.skalny@wearevirtua.com>
2018-09-18 14:50:05 +0200

    VIMS-70, make form hidden for logged users #2

--- app/code/local/Virtua/GuestBook/controllers/IndexController.php
+++ app/code/local/Virtua/GuestBook/controllers/IndexController.php
@@ -10,25 +10,25 @@ class Virtua_GuestBook_IndexController extends Mage_Core_Controller_Front_Action
 
     public function saveAction()
     {
-        $model = Mage::getModel('guestbook/guestbook');
-        $params = Mage::app()->getRequest()->getParams();
-        //if(!empty($params['email']))
-        //Mage::log($params, null, 'guestbook.log', true);
+        if (!Mage::getSingleton('customer/session')->isLoggedIn()) {
 
-        if (!empty($params['name']) && !empty($params['lastname']) && !empty($params['email'])) {
-            $data = [
-                'name' => $params['name'],
-                'lastname' => $params['lastname'],
-                'email' => $params['email'],
-                'ip_address' => '192.168.0.1'
-            ];
+            $model = Mage::getModel('guestbook/guestbook');
+            $params = Mage::app()->getRequest()->getParams();
 
-            $model->setData($data)->save();
-            Mage::getSingleton('core/session')->addSuccess('Success! You are in guest book!');
-        } else {
-            Mage::getSingleton('core/session')->addError('Error! Something went wrong!');
+            if (!empty($params['name']) && !empty($params['lastname']) && !empty($params['email'])) {
+                $data = [
+                    'name' => $params['name'],
+                    'lastname' => $params['lastname'],
+                    'email' => $params['email'],
+                    'ip_address' => '192.168.0.1'
+                ];
+
+                $model->setData($data)->save();
+                Mage::getSingleton('core/session')->addSuccess('Success! You are in guest book!');
+            } else {
+                Mage::getSingleton('core/session')->addError('Error! Something went wrong!');
+            }
         }
-
         $this->_redirect('guestbook');
     }
 }
