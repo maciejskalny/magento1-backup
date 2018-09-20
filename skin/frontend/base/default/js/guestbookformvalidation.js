b1ca1c27d7cdb7246ce35d5c42ad1d8f5aaadbc7
Virtua <m.skalny@wearevirtua.com>
2018-09-20 08:57:59 +0200

    VIMS-70, frontend validation

---
+++ skin/frontend/base/default/js/guestbookformvalidation.js
@@ -0,0 +1,24 @@
+function validateGuestBook()
+{
+    var name = document.getElementById('name').value;
+    var lastname = document.getElementById('lastname').value;
+    var email = document.getElementById('email').value;
+
+    if (validateSimpleText(name) && validateSimpleText(lastname) && validateEmail(email)) {
+        alert('ok');
+        return true;
+    } else {
+        alert("Something went wrong.");
+        return false;
+    }
+}
+
+function validateEmail(email) {
+    var regex = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return regex.test(email);
+}
+
+function validateSimpleText(text) {
+    var regex = /^[A-Z]{1}[a-z]{1,19}$/;
+    return regex.test(text);
+}
