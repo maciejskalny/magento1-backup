ca13c90922747348d7e9e5b5af24cc9c42e1319d
Virtua <m.skalny@wearevirtua.com>
2018-09-21 14:00:57 +0200

    VIMS-76, working on saving csv

---
+++ app/code/local/Virtua/CustomersCsv/Helper/Data.php
@@ -0,0 +1,5 @@
+<?php
+
+class Virtua_CustomersCsv_Helper_Data extends Mage_Core_Helper_Abstract
+{
+}
--- app/code/local/Virtua/CustomersCsv/Model/Observer.php
+++ app/code/local/Virtua/CustomersCsv/Model/Observer.php
@@ -9,8 +9,21 @@ class Virtua_CustomersCsv_Model_Observer
             ->addAttributeToSelect('lastname')
             ->addAttributeToSelect('email');
 
+        $filename = Mage::getBaseDir('var') . DS . 'log' . DS . 'customers.csv';
+        $fh = fopen($filename, "w+");
+
         foreach ($customers as $customer) {
-            Mage::log('dsad', null, 'export.log', true);
+            $row = $customer['firstname'].' '.$customer['lastname'].' '.$customer['email'];
+            fwrite($fh, $row."\n");
         }
+
+        fclose($fh);
     }
+
+
 }
+
+//$filename = Mage::getBaseDir('var') . DS . 'log' . DS . 'customers.csv';
+//$fh = fopen($filename, "a+");
+//fwrite($filename, 'dasdasdasdsad');
+//fclose($fh);
