94371c438c2c2b5f31a2f1224d916439328f86dd
Virtua <m.skalny@wearevirtua.com>
2018-09-24 09:53:22 +0200

    VIMS-76, cleanup

--- app/code/local/Virtua/CustomersCsv/Model/Adminhtml/System/Config/Backend/Model/Cron.php
+++ app/code/local/Virtua/CustomersCsv/Model/Adminhtml/System/Config/Backend/Model/Cron.php
@@ -1,9 +1,31 @@
 <?php
+/**
+ * This file supports cron configuration in admin panel.
+ *
+ * PHP version 7.1.21
+ *
+ * @category  Module
+ * @package   Virtua_Internship
+ * @author    Maciej Skalny <contact@wearevirtua.com>
+ * @copyright 2018 Copyright (c) Virtua (http://wwww.wearevirtua.com)
+ * @license   GPL http://opensource.org/licenses/gpl-license.php
+ * @link      https://bitbucket.org/wearevirtua/magento1ms/
+ */
 
+/**
+ * Class Virtua_CustomersCsv_Model_Adminhtml_System_Config_Backend_Model_Cron
+ */
 class Virtua_CustomersCsv_Model_Adminhtml_System_Config_Backend_Model_Cron extends Mage_Core_Model_Config_Data
 {
+    /**
+     * Constance which holds path to CustomersCsv crontab job.
+     */
     const CRON_STRING_PATH = 'crontab/jobs/customerscsv_cron_task/schedule/cron_expr';
 
+    /**
+     * @return Mage_Core_Model_Abstract|void
+     * @throws Exception
+     */
     protected function _afterSave()
     {
         $time = $this->getData('groups/customerscsv_group/fields/time/value');
@@ -12,13 +34,13 @@ class Virtua_CustomersCsv_Model_Adminhtml_System_Config_Backend_Model_Cron exten
         $frequencyWeekly = Mage_Adminhtml_Model_System_Config_Source_Cron_Frequency::CRON_WEEKLY;
         $frequencyMonthly = Mage_Adminhtml_Model_System_Config_Source_Cron_Frequency::CRON_MONTHLY;
 
-        $cronExprArray = array(
+        $cronExprArray = [
             intval($time[1]),                                   # Minute
             intval($time[0]),                                   # Hour
             ($frequency == $frequencyMonthly) ? '1' : '*',      # Day of the Month
             '*',                                                # Month of the Year
             ($frequency == $frequencyWeekly) ? '1' : '*',       # Day of the Week
-        );
+        ];
         $cronExprString = join(' ', $cronExprArray);
 
         try {
@@ -27,10 +49,8 @@ class Virtua_CustomersCsv_Model_Adminhtml_System_Config_Backend_Model_Cron exten
                 ->setValue($cronExprString)
                 ->setPath(self::CRON_STRING_PATH)
                 ->save();
-        }
-        catch (Exception $e) {
+        } catch (Exception $e) {
             throw new Exception(Mage::helper('cron')->__('Unable to save the cron expression.'));
-
         }
     }
 }
