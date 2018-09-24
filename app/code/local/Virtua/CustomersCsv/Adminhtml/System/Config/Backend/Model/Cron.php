a6b3a520f339cf3b6a0704f0e8d3be5be7ee5ab8
Virtua <m.skalny@wearevirtua.com>
2018-09-24 09:20:17 +0200

    VIMS-76, working on configurable cron

---
+++ app/code/local/Virtua/CustomersCsv/Adminhtml/System/Config/Backend/Model/Cron.php
@@ -0,0 +1,34 @@
+<?php
+
+class Virtua_CustomersCsv_Adminhtml_System_Config_Backend_Model_Cron extends Mage_Core_Model_Config_Data
+{
+    const CRON_STRING_PATH = 'crontab/jobs/customerscsv_cron_task/schedule/cron_expr';
+
+    protected function _afterSave()
+    {
+        $time = $this->getData('groups/customerscsv_group/fields/time/value');
+        $frequency = $this->getValue();
+
+        $frequencyWeekly = Mage_Adminhtml_Model_System_Config_Source_Cron_Frequency::CRON_WEEKLY;
+        $frequencyMonthly = Mage_Adminhtml_Model_System_Config_Source_Cron_Frequency::CRON_MONTHLY;
+
+        $cronExprArray = array(
+            intval($time[1]),                                   # Minute
+            intval($time[0]),                                   # Hour
+            ($frequency == $frequencyMonthly) ? '1' : '*',      # Day of the Month
+            '*',                                                # Month of the Year
+            ($frequency == $frequencyWeekly) ? '1' : '*',       # Day of the Week
+        );
+        $cronExprString = join(' ', $cronExprArray);
+
+        try {
+            Mage::getModel('core/config_data')
+                ->load(self::CRON_STRING_PATH, 'path')
+                ->setValue($cronExprString)
+                ->setPath(self::CRON_STRING_PATH)
+                ->save();
+        } catch (Exception $e) {
+            throw new Exception(Mage::helper('cron')->__('Unable to save the cron expression.'));
+        }
+    }
+}
